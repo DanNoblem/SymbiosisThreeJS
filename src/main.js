13fe84fa302f0d7612e57f43c35394a545f04f9c
DanNoble <danielwai999@gmail.com>
2023-12-13 19:16:02 -0500

    Started Attraction calc

--- src/main.js
+++ src/main.js
@@ -48,11 +48,11 @@ axesHelper.position.y = 0.001;
 scene.add(axesHelper);
 
 // light
-// const ambientLight = new THREE.AmbientLight("white", 0.2);
-// const hemisphereLight = new THREE.HemisphereLight("#ffffff", "#ff00ff", 0.8);
-// const directionalLight = new THREE.DirectionalLight("white", 0.8);
-// directionalLight.position.set(-1, 1, 1);
-// scene.add(ambientLight, directionalLight, hemisphereLight);
+const ambientLight = new THREE.AmbientLight("white", 0.2);
+const hemisphereLight = new THREE.HemisphereLight("#ffffff", "#ff00ff", 0.8);
+const directionalLight = new THREE.DirectionalLight("white", 0.8);
+directionalLight.position.set(-1, 1, 1);
+scene.add(ambientLight, hemisphereLight);
 
 // control
 const orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -88,30 +88,46 @@ window.addEventListener("resize", onResize);
 DRAWING//////////////////////////////////////////////////////////////////////////////
 */
 
-var balls = [];
-var mass = [];
+let balls = [];
+let mass = [];
+let vel = [];
+let acc = [];
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+let food;
+
+const geometry = new THREE.BoxGeometry(4, 4, 4);
+const foodMaterial = new THREE.MeshStandardMaterial({
+  color: "red",
+  roughness: 0.2,
+  metalness: 0.8,
+});
 //Sphere rendering properties
 const sphereMaterial = new THREE.MeshNormalMaterial();
-const sphereGeometry = new THREE.SphereGeometry(2, 128, 128);
+const sphereGeometry = new THREE.SphereGeometry(2, 64, 64);
 
 //create spheres
 for (let i = 0; i < 50; i++) {
   balls[i] = new THREE.Mesh(sphereGeometry, sphereMaterial.clone());
   mass[i] = Math.random() * 10;
-  const x = 100 - Math.random() * 200;
-  const y = 100 - Math.random() * 200;
-  const z = 100 - Math.random() * 200;
+  const x = 50 - Math.random() * 100;
+  const y = 50 - Math.random() * 100;
+  const z = 50 - Math.random() * 100;
   balls[i].position.set(x, y, z);
+  vel[i] = new THREE.Vector3(x, y, z);
+  acc[i] = new THREE.Vector3(0, 0, 0);
 
   // adding custome properties to use later
   balls[i].name = "sphere";
   balls[i].isAnimating = false;
 
   scene.add(balls[i]);
-  objects.push(balls[i]);
 }
+
+//create food
+food = new THREE.Mesh(geometry, foodMaterial.clone());
+food.position.set(0, 0, 0);
+objects.push(food);
+scene.add(food);
 
 /* 
 //////////////////////////////////////////////////////////////////////////////
@@ -152,7 +168,13 @@ function render() {
 
 //animation
 const animate = () => {
-  for (let i = 0; i < balls.length; i++) {}
+  for (let i = 0; i < balls.length; i++) {
+    //gravity calculation
+    //attraction
+    let force = balls[i].position;
+    let dist = balls[i].position.distanceTo(food.position);
+    let power = 
+  }
   requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
